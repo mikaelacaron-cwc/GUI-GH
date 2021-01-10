7ea8131963507f9a8ebcfb3ba7b546fc0412e1c9
Mikaela Caron <mikaelacaron@gmail.com>
2021-01-09 23:56:55 -0500

    Initial Commit

---
+++ GUI-GH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GUI-GH
+//
+//  Created by Mikaela Caron on 1/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GUI-GH/GUI_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  GUI_GHApp.swift
+//  GUI-GH
+//
+//  Created by Mikaela Caron on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct GUI_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
