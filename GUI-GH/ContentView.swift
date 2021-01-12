7312a9d431e23db1840979f8863d905b8bcbe1ad
mikaelacaron-cwc <75949443+mikaelacaron-cwc@users.noreply.github.com>
2021-01-11 23:21:16 -0500

    Same team color

--- GUI-GH/ContentView.swift
+++ GUI-GH/ContentView.swift
@@ -11,9 +11,9 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
-                Image(systemName: "figure.wave").resizable().foregroundColor(.yellow).frame(width: 100, height: 150, alignment: .center)
-                Image(systemName: "figure.wave").resizable().foregroundColor(.purple).frame(width: 100, height: 150, alignment: .center)
-                Image(systemName: "figure.wave").resizable().foregroundColor(.green).frame(width: 100, height: 150, alignment: .center)
+                Image(systemName: "figure.wave").resizable().foregroundColor(.blue).frame(width: 100, height: 150, alignment: .center)
+                Image(systemName: "figure.wave").resizable().foregroundColor(.blue).frame(width: 100, height: 150, alignment: .center)
+                Image(systemName: "figure.wave").resizable().foregroundColor(.blue).frame(width: 100, height: 150, alignment: .center)
             }
             
             Text("What's up, CodeCrew!")
