2847cb2ae37d353d073ef2fb29c4c2868b550b3b
Mikaela Caron <mikaelacaron@gmail.com>
2021-01-11 23:10:19 -0500

    Add team members to CodeCrew

--- GUI-GH/ContentView.swift
+++ GUI-GH/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "figure.wave").resizable().foregroundColor(.green).frame(width: 100, height: 150, alignment: .center)
+            HStack {
+                Image(systemName: "figure.wave").resizable().foregroundColor(.yellow).frame(width: 100, height: 150, alignment: .center)
+                Image(systemName: "figure.wave").resizable().foregroundColor(.purple).frame(width: 100, height: 150, alignment: .center)
+                Image(systemName: "figure.wave").resizable().foregroundColor(.green).frame(width: 100, height: 150, alignment: .center)
+            }
+            
             Text("What's up, CodeCrew!")
                 .padding()
         }
